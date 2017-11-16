4bd2377a897e1b9095628ed78dbb047054918e36
Jake Poirier <33732144+J-Poirier@users.noreply.github.com>
2017-11-16 14:08:32 -0800

    fixed level scene loader

--- Assets/Scripts/scChanger.cs
+++ Assets/Scripts/scChanger.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class scChanger : MonoBehaviour {
-
+    /* 
+     * loads correct scene on button click 
+     * to add a new scene make new method with scene name in ""
+    */
 	
 	public void maninMenu ()
     {
@@ -20,7 +23,7 @@ public class scChanger : MonoBehaviour {
     }
     public void newGame()
     {
-        SceneManager.LoadScene("level");
+        SceneManager.LoadScene("level1");
     }
     
     public void howTo()
