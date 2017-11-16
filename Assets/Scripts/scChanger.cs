631043288853cf46464b8cd5516f78742bf66c44
Jake Poirier <33732144+J-Poirier@users.noreply.github.com>
2017-11-16 14:02:40 -0800

    Added basic scene function

---
+++ Assets/Scripts/scChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scChanger : MonoBehaviour {
+
+	
+	public void maninMenu ()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+    public void highScroes ()
+    {
+        SceneManager.LoadScene("highScores");
+    }
+    public void about()
+    {
+        SceneManager.LoadScene("about");
+    }
+    public void newGame()
+    {
+        SceneManager.LoadScene("level");
+    }
+    
+    public void howTo()
+    {
+        SceneManager.LoadScene("howToPlay");
+    }
+}
