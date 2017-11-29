1173ed5cc0b93cd5085e17045c6842426986f6f7
hghart <henry_hart4@hotmail.com>
2017-11-29 14:05:54 -0800

    Ai added
    made a new scene with the ai test on it, mario jumps on turtle and it turns into a shell, still need to add the actual turtle we are going to use with animations...

---
+++ Assets/Scripts/AiMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiMovement : MonoBehaviour {
+
+	public int speed;
+	public int xpos;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, new Vector2 (xpos, 0));
+		rb.AddForce (Vector2.right * speed * xpos);
+		if (hit.distance < 3.0f) {
+			flipAi ();
+			if (hit.collider.tag == "Player"){
+				Destroy (hit.collider.gameObject);
+			}
+		}
+	}
+
+	void flipAi (){
+		xpos = xpos * -1;
+	}
+}
