adde4cfaec4701b3d6fdc8b79439be8d555e9530
mskolos <maryskolos@gmail.com>
2017-12-08 19:07:46 -0800

    Added button behaviour on tutorial level

---
+++ Assets/Scripts/TriggerBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBehaviour : MonoBehaviour {
+    public GameObject column;
+
+    void Start()
+    {
+        Debug.Log(this.name + " " + this.tag);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (this.tag == "reverse_trigger")
+        {
+            if (other.tag == "gem")
+            {
+                column.GetComponent<Renderer>().enabled = false;
+            }
+        }
+        else
+        {
+            if (other.tag == "gem")
+            {
+                column.GetComponent<Renderer>().enabled = true;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (this.tag == "reverse_trigger")
+        {
+            if (other.tag == "gem")
+            {
+                column.GetComponent<Renderer>().enabled = true;
+            }
+        }
+        else
+        {
+            if (other.tag == "gem")
+            {
+                column.GetComponent<Renderer>().enabled = false;
+            }
+        }
+    }
+}
